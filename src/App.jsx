e148a83f331c8855931a1d9360aba24b916f1710
BGISS <bissgirish22@gmail.com>
2025-06-08 12:55:46 -0400

    changed some paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,7 @@ function App() {
 
   return (
     <>
-  <BrowserRouter>
-    <Routes>
-    <Route path="/"  element={<Application/>}/>
-    </Routes>
-      </BrowserRouter>
+      <Application/>
     </>
   )
 }
